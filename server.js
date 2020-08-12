3c809d63ceb0288a5c23d3676de4000bbfb9f89c
KunZizinho <kun.zizinho@gmail.com>
2020-08-11 21:46:42 -0400

    test

--- server.js
+++ server.js
@@ -18,10 +18,11 @@ app.use(express.static('public'));
 
 // setting up mongo db
 var mongoDB_uri = process.env.mongoDB_uri || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(mongoDB_uri, {
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
+// mongoose.connect(mongoDB_uri, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false
+// })
+mongoose.connect(mongoDB_uri)
 
 //routes
 require("./routes/apiRoutes")(app);
