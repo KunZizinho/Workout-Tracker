db0392e56025d3da037d02947ca5ff6370221e7d
KunZizinho <kun.zizinho@gmail.com>
2020-08-11 23:28:52 -0400

    deployment

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // setting up mongo db
-var mongoDB_uri = process.env.mongoDB_uri || "mongodb://localhost/workouts";
+var mongoDB_uri = process.env.mongoDB_uri || "mongodb://localhost/workout";
 mongoose.connect(mongoDB_uri, {
     useNewUrlParser: true,
     useFindAndModify: false
