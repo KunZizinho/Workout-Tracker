e77439599fe1eee7cd68740a722b5dfd9593dccf
KunZizinho <kun.zizinho@gmail.com>
2020-08-21 04:13:38 -0400

    working on mlab

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 
+
 // Setting up Express App
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -18,10 +19,14 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // setting up mongo db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-})
+mongoose.connect(process.env.mongoDB_uri || "mongodb://localhost/workout",{
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+// mongoose.connect(mongoDB_uri, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false
+// })
 
 //routes
 require("./routes/apiRoutes")(app);
